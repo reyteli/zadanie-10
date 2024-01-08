01101d068834e2a1c0d8607c3972f17398850ef6
Никита Олегович Романенко <nikcom526@mail.ru>
2024-01-08 19:41:26 +0300

    Добавьте файлы проекта.

---
+++ zadanie 10/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace zadanie_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float gradus = (float)Convert.ToDouble(Console.ReadLine());
+            float min = (float)Convert.ToDouble(Console.ReadLine());
+            float sec = (float)Convert.ToDouble(Console.ReadLine());
+            ugol ugol = new ugol(gradus,min,sec);
+            float TotalRadians = ugol.ToRadians();
+            Console.WriteLine( TotalRadians);
+            Console.ReadKey();
+        }
+        
+    }
+}
---
+++ zadanie 10/ugol.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadanie_10
+{
+    class ugol
+    {
+        float gradus;
+        float min;
+        float sec;
+
+        public float Gradus
+        {
+            get
+            {
+                return gradus;
+            }
+            set
+            {
+                if(value<0)
+                    {
+                    Console.WriteLine("Значение не корректно");
+                }
+                else
+                {
+                    gradus = value;
+                }
+
+            }
+        }
+        public float Min
+        {
+            get
+            {
+                return min;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    Console.WriteLine("Значение не корректно");
+                }
+                else
+                {
+                   min = value;
+                }
+
+            }
+        }
+        public float Sec
+        {
+            get
+            {
+                return sec;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    Console.WriteLine("Значение не корректно");
+                }
+                else
+                {
+                    sec = value;
+                }
+
+            }
+        }
+
+        public ugol(float gradus, float min, float sec)
+        {
+            this.gradus = gradus;
+            this.min = min;
+            this.sec = sec;
+        }
+       public float ToRadians()
+        {
+            return ( sec /  206265 + min / 3438 + gradus / 57);
+        }
+    }
+}
